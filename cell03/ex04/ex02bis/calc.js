83cbfb9aba7967174fbef6f50cf8e16d63ce736d
pawatsri <154305429+pawatsri@users.noreply.github.com>
2024-09-12 11:05:23 +0700

    Add files via upload

---
+++ cell03/ex04/ex02bis/calc.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+    $('#button').on('click', function() {
+        const leftNumber = parseFloat($('#leftNumber').val()); 
+        const rightNumber = parseFloat($('#rightNumber').val()); 
+        const operator = $('#operator').val();
+        let result;
+
+        if (isNaN(leftNumber) || isNaN(rightNumber) || leftNumber < 0 || rightNumber < 0) {
+            alert("Error :( ");
+            return;
+        }
+        switch (operator) {
+            case "+":
+                result = leftNumber + rightNumber;
+                alert("The answer is: " + result);
+                break;
+            case "-":
+                result = leftNumber - rightNumber;
+                alert("The answer is: " + result);
+                break;
+            case "*":
+                result = leftNumber * rightNumber;
+                alert("The answer is: " + result);
+                break;
+            case "/":
+                if (rightNumber == 0) {
+                    alert("It's over 9000!");
+                } else {
+                    result = leftNumber / rightNumber;
+                    alert("The answer is: " + result);
+                }
+                break;
+            case "%":
+                if (rightNumber == 0) {
+                    alert("It's over 9000!");
+                } else {
+                    result = leftNumber % rightNumber;
+                    alert("The answer is: " + result);
+                }
+                break;
+            default:
+                alert("Invalid operator");
+                break;
+        }
+    });
+    setInterval(() => {
+        alert('Please, use me...');
+    }, 30000);
+});
